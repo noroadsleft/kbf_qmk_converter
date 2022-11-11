bbd79208276e3282c8a7dc483f5ba9c177e15bd0
Ryan <fauxpark@gmail.com>
2022-11-12 09:13:34 +1100

    Bootstrap redesign (#13)
    * Bootstrap redesign
    
    * Keep original UC keycodes for now

--- readSingleFile.js
+++ readSingleFile.js
@@ -1,26 +1,26 @@
 function readSingleFile(evt) {
-	//Retrieve the first (and only!) File from the FileList object
-	var f = evt.target.files[0];
+  //Retrieve the first (and only!) File from the FileList object
+  var f = evt.target.files[0];
 
-	if (f) {
-		var r = new FileReader();
-		r.onload = function(e) {
-			var contents = e.target.result;
-			var metadata = [
-				"name: " + f.name,
-				"type: " + f.type,
-				"size: " + f.size,
-				"starts with: " + contents.substr(1, contents.indexOf("\n"))
-			];
-			//console.log( "Got the file:\n"+ metadata.join('\n') );
-			document.getElementById('input').value = contents;
-			document.getElementById('submit').click();
-		}
-		r.readAsText(f);
-		//console.log( typeof r.readAsText(f) );
-	} else {
-		alert("Failed to load file");
-	}
+  if (f) {
+    var r = new FileReader();
+    r.onload = function(e) {
+      var contents = e.target.result;
+      var metadata = [
+        "name: " + f.name,
+        "type: " + f.type,
+        "size: " + f.size,
+        "starts with: " + contents.substr(1, contents.indexOf("\n"))
+      ];
+      //console.log( "Got the file:\n"+ metadata.join('\n') );
+      document.querySelector('#input').value = contents;
+      document.querySelector('#submit').click();
+    }
+    r.readAsText(f);
+    //console.log( typeof r.readAsText(f) );
+  } else {
+    alert("Failed to load file");
+  }
 }
 
-document.getElementById('fileImport').addEventListener('change', readSingleFile, false);
+document.querySelector('#file-import').addEventListener('change', readSingleFile, false);
